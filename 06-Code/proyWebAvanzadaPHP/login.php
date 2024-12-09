f8b3835b1e8765102004ec1f165535932f396c31
JosueXDa <josueloy12@gmail.com>
2024-12-09 07:20:03 -0500

    correct ogin

--- 06-Code/proyWebAvanzadaPHP/login.php
+++ 06-Code/proyWebAvanzadaPHP/login.php
@@ -15,18 +15,25 @@
     
     <div class="container pt-4">
         <form action="./auth.php" method="POST" class="login-container form-control">
-
-            <h1 class="h3 mb-3 fw-normal">Accede a tu cuenta del club CPED</h1>
-            <div class="form-floating mb-3">
-              <input type="text" class="form-control" id="username" name="username" placeholder="Username">
-              <label for="username">Usuario</label>
-            </div>
-            <div class="form-floating mb-3">
-              <input type="password" class="form-control" id="passwordLogin" name="passwordLogin" placeholder="Password">
-              <label for="passwordLogin">Contraseña</label>
-            </div>
-            <button class="btn btn-primary w-100 py-2" type="submit">Iniciar Sesion</button>
-        </form>
+          <?php
+            if (isset($_GET['status']) && $_GET['status'] === 'error') {
+                echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>¡Error!</strong> Verifica usuario o contraseña.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+            }
+          ?>
+          <h1 class="h3 mb-3 fw-normal">Accede a tu cuenta del club CPED</h1>
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+            <label for="username">Usuario</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="password" class="form-control" id="passwordLogin" name="passwordLogin" placeholder="Password">
+            <label for="passwordLogin">Contraseña</label>
+          </div>
+          <button class="btn btn-primary w-100 py-2" type="submit">Iniciar Sesion</button>
+      </form>
     </div>
     <div class="modal fade" id="modal-dialog-msg" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -48,5 +55,6 @@
     <script src="./assets/scriptsJs/menu.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/scriptsJs/resetQuerys.js"></script>
 </body>
 </html>
