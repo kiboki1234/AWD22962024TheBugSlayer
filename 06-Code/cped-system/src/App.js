b26a1fd9ad640137b3ae8458cc4c289772b16f42
Josue Abel Guallichico Loya <81202019+JosueXDa@users.noreply.github.com>
2025-01-20 18:56:11 -0500

    th files of landing page was completed

--- 06-Code/cped-system/src/App.js
+++ 06-Code/cped-system/src/App.js
@@ -1,12 +1,25 @@
 //import logo from './logo.svg';
 import './App.css';
-import LandingPage from './components/LandingPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutUs from './components/pages/AboutUs';
+import Contact from './components/pages/Contacts';
+import Events from './components/pages/Events';
+import Login from './components/pages/Login';
+import DebateModels from './components/pages/DebateModels';
 
 function App() {
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/debate-models" element={<DebateModels />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
