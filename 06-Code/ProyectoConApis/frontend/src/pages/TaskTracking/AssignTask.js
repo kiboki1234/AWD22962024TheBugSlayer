08b67694d48df81094554bb5f6985dfbda46268e
Josue Abel Guallichico Loya <81202019+JosueXDa@users.noreply.github.com>
2025-01-22 23:30:08 -0500

    first attemp for task tracking jja

--- 06-Code/ProyectoConApis/frontend/src/pages/TaskTracking/AssignTask.js
+++ 06-Code/ProyectoConApis/frontend/src/pages/TaskTracking/AssignTask.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import API_BASE_URL from '../../api';
 
 const AssignTask = ({ handleAssignTask }) => {
+
+  const [leaders, setLeaders] = useState([]);
+
+  useEffect(() => {
+    const fetchLeaders = async () => {
+      try {
+        const response = await axios.get(`${API_BASE_URL}/api/leaders`);
+        setLeaders(response.data);
+      } catch (error) {
+        console.error("Error al obtener los líderes:", error);
+      }
+    };
+
+    fetchLeaders();
+  }, []);
   return (
     <div className="col-md-6 mb-4">
       <h2>Asignar Tarea</h2>
@@ -15,7 +32,14 @@ const AssignTask = ({ handleAssignTask }) => {
         </div>
         <div className="mb-3">
           <label htmlFor="assignedTo" className="form-label">Asignar a</label>
-          <input type="text" id="assignedTo" name="assignedTo" className="form-control" required />
+          <select id="assignedTo" name="assignedTo" className="form-control" required>
+            <option value="">Selecciona un usuario</option>
+            {leaders.map((leader) => (
+              <option key={leader._id} value={leader._id}>
+                {leader.assignedMembers.map(member => member.name).join(', ')}
+              </option>
+            ))}
+          </select>
         </div>
         <button type="submit" className="btn btn-primary w-100">Asignar Tarea</button>
       </form>
